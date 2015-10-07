ffa57646e38aa2545178e579ec1481eef8e7db8d
maoshuai <maoshuai@maoshuaideMacBook-Pro.local>
2015-10-07 16:48:07 +0800

    post add tag

--- www/static/src/admin/components/PostAddTags.jsx
+++ www/static/src/admin/components/PostAddTags.jsx
@@ -12,7 +12,7 @@ class PostAddTags extends BaseComponent {
         super(props);
 
         this.state = {
-            name: ''
+            tags: '测试'
         };
     }
 
@@ -25,21 +25,29 @@ class PostAddTags extends BaseComponent {
     render() {
 
         return (
-            <div>
-                填写标签
+            <div className="tag-wrapper">
+                <p className="tag-title">添加文章标签</p>
+                <div className="tag-content">
+                    <input type="text" placeholder="标签间请用“空格”隔开，最多可添加3个标签。" value={this.state.tags} onInput={this.handleChange}/>
+                </div>
+                <div className="tag-suggest">
+                    <dl>
+                        <dt>常用标签</dt>
+                        <dd>
+                            <a href="javascript:;" onClick={this.handleChoose}>标签</a>
+                            <a href="javascript:;" onClick={this.handleChoose}>测试</a>
+                            <a href="javascript:;" onClick={this.handleChoose}>javascript</a>
+                            <a href="javascript:;" onClick={this.handleChoose}>css</a>
+                        </dd>
+                    </dl>
+                </div>
             </div>
         )
     }
 
     handleSave(e) {
         e.preventDefault();
-        TagActions.add({name: this.state.name});
-    }
-
-    handleChange(e) {
-        this.setState({
-            name: e.target.value
-        });
+        TagActions.add({name: this.state.names});
     }
 
     onStatusChange(status) {
@@ -48,9 +56,45 @@ class PostAddTags extends BaseComponent {
                 AlertActions.error('标签创建失败: ' + status.error);
             } else {
                 AlertActions.success('标签创建成功');
-                this.setState({name: ''});
+                this.setState({tags: ''});
             }
         }
+    }
+
+    handleChange(e) {
+        let tagCache = [];
+        let tags = e.target.value.split(/\s+/);
+        if(tags.length > 3) {
+            AlertActions.error('标签最多添加3个');
+            return;
+        }
+        for(let tag of tags) {
+            if(tagCache.indexOf(tag) == -1) {
+                tagCache.push(tag);
+            }else {
+                AlertActions.error('该标签已使用，无需重复填写！');
+            }
+        }
+        this.setState({
+            tags: tagCache.join(' ')
+        });
+    }
+
+    handleChoose(e) {
+        let tags = this.state.tags.split(/\s+/);
+        let newTag = e.target.innerHTML;
+        if(tags.length >= 3) {
+            AlertActions.error('标签最多添加3个');
+            return;
+        }
+        if(tags.indexOf(newTag) > -1) {
+            AlertActions.error('该标签已选择，无需重复选择！');
+            return;
+        }
+        tags.push(newTag);
+        this.setState({
+            tags: tags.join(' ')
+        });
     }
 }
 
