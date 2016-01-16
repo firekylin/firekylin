f5a8492e3c7b2b9dbfcf40f4abaafa605fd8d0a3
lichengyin <welefen@gmail.com>
2016-01-16 15:19:03 +0800

    update

---
+++ src/common/controller/base.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * base controller
+ */
+export default class extends think.controller.base {
+  /**
+   * get website options
+   * @return {Promise} []
+   */
+  getOptions(){
+    
+  }
+}
--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -2,6 +2,7 @@
 
 import Base from './base.js';
 
+
 export default class extends Base {
   /**
    * index action
