75d043d18572049c6783edc2b4d580f46eb38d85
lichengyin <welefen@gmail.com>
2016-01-29 11:37:48 +0800

    update

--- src/admin/controller/index.js
+++ src/admin/controller/index.js
@@ -11,4 +11,20 @@ export default class extends Base {
     //auto render template file index_index.html
     return this.display();
   }
+  /**
+   * [summartyAction description]
+   * @return {[type]} [description]
+   */
+  async summaryAction(){
+    let model = this.model('post');
+    let allCountPromise = model.getCount();
+    let userCountPromise = model.getCount(this.userInfo.id);
+    let latestPromise = model.getLatest();
+    let [allCount, userCount, latest] = await Promise.all([allCountPromise, userCountPromise, latestPromise]);
+    return this.success({
+      allCount,
+      userCount,
+      latest
+    })
+  }
 }
