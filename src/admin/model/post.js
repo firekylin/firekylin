4c8467ad3c032843f054fffa170fdaa23778383d
lizheming <secretlzm007@gmail.com>
2016-02-23 23:28:07 +0800

    初步增加文章列表和新增编辑页面

--- src/admin/model/post.js
+++ src/admin/model/post.js
@@ -13,9 +13,39 @@ export default class extends think.model.relation {
 
     this.relation = {
       tag: think.model.MANY_TO_MANY,
-      cate: think.model.MANY_TO_MANY
+      cate: think.model.MANY_TO_MANY,
+      user: {
+        type: think.model.BELONG_TO,
+        fKey: 'user_id',
+        key: 'display_name',
+        field: 'id,display_name'
+      }
     }
   }
+
+  /**
+   * 添加文章
+   * @param {[type]} data [description]
+   * @param {[type]} ip   [description]
+   */
+  addPost(data){
+    let create_time = think.datetime();
+    return this.where({pathname: 'hello-world', _logic: 'OR'}).thenAdd({
+      user_id: 1,
+      type: 0,
+      status: 0,
+      title: data.title,
+      pathname: 'hello-world',
+      summary: 'abc',
+      markdown_content: 'abc',
+      content: 'abc',
+      allow_comment: 1,
+      create_time,
+      update_time: create_time,
+      is_public: 1
+    });
+  }
+
   /**
    * get count posts
    * @param  {Number} userId []
