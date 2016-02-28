8f83ff077667b63c8240bdff7dc3d6a654cf4acb
lichengyin <welefen@gmail.com>
2016-02-28 14:42:46 +0800

    update

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -4,14 +4,7 @@ import Base from './base.js';
 
 
 export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  async indexAction(){
-    let model = this.model('post');
-    let list = await model.getPostList(this.get('page'), {});
-    this.assign('postList', list);
-    return this.display();
+  indexAction(){
+    return this.action('post', 'list');
   }
 }
