c956ae245cec63d3f9b280158160743d52eeb019
lichengyin <welefen@gmail.com>
2016-02-29 11:19:22 +0800

    update

--- src/home/config/route.js
+++ src/home/config/route.js
@@ -3,6 +3,7 @@ export default [
   [/^about/, 'post/page?pathname=about'],
   [/^archives/, 'post/archive'],
   [/^tags/, 'post/tag'],
+  [/^links/, 'post/page?pathname=links'],
   [/^rss/, 'post/rss'],
   [/^page\/([^/]+)/, 'post/page?pathname=:1'],
   [/^post\/([^/]+)/, 'post/detail?pathname=:1']  
