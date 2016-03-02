2e61ec6dbc85e75fd5154e9f1926d07dcc49a42a
lichengyin <welefen@gmail.com>
2016-03-02 17:20:15 +0800

    support theme

--- src/home/controller/base.js
+++ src/home/controller/base.js
@@ -2,6 +2,16 @@
 
 export default class extends think.controller.base {
   /**
+   * init
+   * @param  {[type]} http [description]
+   * @return {[type]}      [description]
+   */
+  init(http){
+    super.init(http);
+    //home view path
+    this.HOME_VIEW_PATH = `${think.ROOT_PATH}${think.sep}view${think.sep}home${think.sep}`;
+  }
+  /**
    * some base method in here
    */
   async __before(){
@@ -10,8 +20,21 @@ export default class extends think.controller.base {
     this.options = options;
     this.assign('options', options);
 
+    //set theme view root path
+    let theme = options.theme || 'firekylin';
+    this.THEME_VIEW_PATH = `${think.ROOT_PATH}${think.sep}www${think.sep}theme${think.sep}${theme}${think.sep}`;
+
+    //set host
     let protocal = options.is_https ? 'https://' : 'http://';
     let host = protocal + this.http.host;
     this.assign('host', host);
+  }
+  /**
+   * display view page
+   * @param  {} name []
+   * @return {}      []
+   */
+  display(name){
+    return super.display(this.THEME_VIEW_PATH + name + '.html');
   }
 }
