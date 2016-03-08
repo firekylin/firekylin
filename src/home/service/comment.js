49171f13675520fb6fd6376a2730425228b6b18a
lichengyin <welefen@gmail.com>
2016-03-08 10:06:26 +0800

    支持从 disqus 同步评论数

--- src/home/service/comment.js
+++ src/home/service/comment.js
@@ -25,24 +25,56 @@ export default class extends think.service.base {
     }
   }
   /**
+   * get post data
+   * @return {[type]} [description]
+   */
+  async getPostData(){
+    let postModel = this.model('post');
+    let allPost = await postModel.setRelation(false).order('create_time DESC').field('id,pathname,comment_num').select();
+    let keys = {};
+    allPost.map(item => {
+      let key = think.md5(item.pathname);
+      keys[key] = {id: item.id, comment_num: item.comment_num};
+      return key;
+    });
+    return keys;
+  }
+  /**
    * sync from disqus
    * @return {[type]} [description]
    */
-  syncFromDisqus(comment){
-    
+  async syncFromDisqus(comment){
+
+    let postData = await this.getPostData();
+    let threads = Object.keys(postData).join('&l=');
+    let url = `https://${comment.name}.disqus.com/count-data.js?1=${threads}`;
+    think.log(`sync comments from ${url}`);
+    let fn = think.promisify(request, request);
+    let response = await fn(url);
+    let data = response.body.match(/DISQUSWIDGETS.displayCount\(([^\(\)]+)\);/);
+    if(!data){
+      return;
+    }
+
+    data = JSON.parse(data[1]).counts;
+    let promises = [];
+    data.forEach(item => {
+      if(item.comments === postData[item.id].comment_num){
+        return;
+      }
+      let id = postData[item.id].id;
+      let promise = this.model('post').where({id: id}).update({comment_num: item.comments});
+      promises.push(promise);
+    })
+    return Promise.all(promises);
   }
   /**
    * sync from duoshuo
    * @return {[type]} [description]
    */
   async syncFromDuoshuo(comment){
-    let allPost = await this.model('post').setRelation(false).field('id,pathname,comment_num').select();
-    let keys = {};
-    let threads = allPost.map(item => {
-      let key = think.md5(item.pathname);
-      keys[key] = {id: item.id, comment_num: item.comment_num};
-      return key;
-    }).join(',');
+    let postData = await this.getPostData();
+    let threads = Object.keys(postData).join(',');
     let url = `http://api.duoshuo.com/threads/counts.json?short_name=${comment.name}&threads=${threads}`;
     think.log(`sync comments from ${url}`);
     let fn = think.promisify(request, request);
@@ -50,10 +82,10 @@ export default class extends think.service.base {
     let data = JSON.parse(response.body).response;
     let promises = [];
     for(let key in data){
-      if(data[key].comments === keys[key].comment_num){
+      if(data[key].comments === postData[key].comment_num){
         continue;
       }
-      let id = keys[key].id;
+      let id = postData[key].id;
       let promise = this.model('post').where({id: id}).update({comment_num: data[key].comments});
       promises.push(promise);
     }
