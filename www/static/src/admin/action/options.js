a14940c4377e4b47fb76acb0f9dcf669b6940d6f
lichengyin <welefen@gmail.com>
2016-03-23 12:21:30 +0800

    Update 2FA

--- www/static/src/admin/action/options.js
+++ www/static/src/admin/action/options.js
@@ -4,6 +4,7 @@ import Reflux from 'reflux';
 let AsyncConfig = {asyncResult: true};
 export default Reflux.createActions({
   save: {children: ['completed', 'failed'], asyncResult: true},
+  auth: {children: ['completed', 'failed'], asyncResult: true},
   qrcode: {children: ['completed', 'failed'], asyncResult: true},
   comment: {children: ['completed', 'failed'], asyncResult: true}
 });
