fe1e5e09b2df3d6bd839bc7ba665674f3665b209
lichengyin <welefen@gmail.com>
2016-03-23 12:21:30 +0800

    Update 2FA

--- src/common/config/locale/en.js
+++ src/common/config/locale/en.js
@@ -8,5 +8,7 @@ export default {
   DATA_EMPTY: [103, '数据不能为空'],
   ACCOUNT_ERROR: [104, '用户名或者密码错误'],
   CATE_EXIST: [105, '分类名称或者缩略名已经存在'],
-  TAG_EXIST: [106, '标签名称或者缩略名已经存在']
+  TAG_EXIST: [106, '标签名称或者缩略名已经存在'],
+  TWO_FACTOR_AUTH_ERROR: [107, '两步校验码错误'],
+  TWO_FACTOR_AUTH_ERROR_DETAIL: [107, '两步校验码错误，请确认校验码或服务器时间是否正确']
 };
