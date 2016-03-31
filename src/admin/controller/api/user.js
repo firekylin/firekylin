3ebf4b162c7ec62d230a7a66864398e92f63ea5b
lizheming <secretlzm007@gmail.com>
2016-03-31 10:00:46 +0800

    用户详情页增加 generate key 操作

--- src/admin/controller/api/user.js
+++ src/admin/controller/api/user.js
@@ -8,17 +8,35 @@ export default class extends Base {
    * @return {[type]} [description]
    */
   getAction(self){
-    this.modelInstance.field('id,name,display_name,email,type,status,create_time,last_login_time');
+    this.modelInstance.field('id,name,display_name,email,type,status,create_time,last_login_time,app_key,app_secret');
     return super.getAction(self);
   }
   /**
    * add user
    * @return {[type]} [description]
    */
-  async postAction(){
+  async postAction(self){
+    if( this.get('type') === 'key' ) {
+      return await this.generateKey(self);
+    }
+
     let data = this.post();
     let insertId = await this.modelInstance.addUser(data, this.ip());
     return this.success({id: insertId});
+  }
+
+  async generateKey(self) {
+    let isAdmin = this.userInfo.type === firekylin.USER_ADMIN;
+    let isMine = this.userInfo.id === this.id;
+    if( !isAdmin && !isMine ) {
+      return this.failed();
+    }
+
+    let app_key = think.uuid();
+    let app_secret = think.uuid();
+
+    this.modelInstance.generateKey(this.id, app_key, app_secret);
+    return await this.getAction(self);
   }
   /**
    * update user info
--- www/static/src/admin/action/user.js
+++ www/static/src/admin/action/user.js
@@ -8,4 +8,5 @@ export default Reflux.createActions({
   save: {children: ['completed', 'failed'], asyncResult: true},
   savepwd: {children: ['completed', 'failed'], asyncResult: true},
   login: {children: ['completed', 'failed'], asyncResult: true},
+  generateKey: {children: ['completed', 'failed'], asyncResult: true}
 });
