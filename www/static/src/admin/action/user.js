e2d8c269e2fb305de35403e8f452e844f2c8d421
lizheming <secretlzm007@gmail.com>
2016-04-04 10:19:57 +0800

    用户列表页增加待审核 Tab 并增加通过和拒绝操作

--- www/static/src/admin/action/user.js
+++ www/static/src/admin/action/user.js
@@ -5,6 +5,7 @@ let AsyncConfig = {asyncResult: true};
 export default Reflux.createActions({
   select: {children: ['completed', 'failed']},
   delete: {children: ['completed', 'failed']},
+  pass: {children: ['completed', 'failed']},
   save: {children: ['completed', 'failed'], asyncResult: true},
   savepwd: {children: ['completed', 'failed'], asyncResult: true},
   login: {children: ['completed', 'failed'], asyncResult: true},
--- www/static/src/admin/component/user_list.jsx
+++ www/static/src/admin/component/user_list.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Base from 'base';
 import {Link} from 'react-router';
 import classnames from 'classnames';
+import {
+  Col,
+  Tabs,
+  Tab
+} from 'react-bootstrap';
 
 import BreadCrumb from 'admin/component/breadcrumb';
 import UserAction from '../action/user';
@@ -16,7 +21,8 @@ export default class extends Base {
     super(props);
     this.state = {
       loading: true,
-      userList: []
+      userList: [],
+      key: 0
     }
   }
   componentDidMount(){
@@ -27,9 +33,9 @@ export default class extends Base {
     switch(type){
       case 'deleteUserSuccess':
         TipAction.success('删除成功');
-        UserAction.select();
+        UserAction.select(null, this.state.key===3?'contributor':'');
         break;
-      default: 
+      default:
         this.setState({userList: data, loading: false});
     }
   }
@@ -38,6 +44,30 @@ export default class extends Base {
       UserAction.delete(userId);
     }, 'modal-sm');
   }
+  handleSelect(type) {
+    this.state.key = type;
+    this.state.page = 1;
+    return UserAction.select(null, type===3?'contributor':'');
+  }
+  pass(user) {
+    UserAction.pass(user.id);
+  }
+
+  deny(user) {
+    return ModalAction.confirm('提示', <div className="center">拒绝该用户的申请后会直接删除该账号<br /><p className="gray">删除后无法恢复</p></div>, () => {
+      UserAction.delete(user.id);
+    }, 'modal-sm');
+  }
+
+  getUserType(user) {
+    switch(user.type) {
+      case 1: return '管理员'; break;
+      case 2: return '编辑'; break;
+      case 3: return '投稿者'; break;
+    }
+    return '';
+  }
+
   getUserList(){
     if(this.state.loading){
       return (<tr><td colSpan="8" className="center">加载中。。。</td></tr>);
@@ -51,19 +81,30 @@ export default class extends Base {
           <td scope="row">{item.id}</td>
           <td>{item.display_name || item.name}</td>
           <td>{item.email}</td>
-          <td>{item.type == 1 ? '管理员' : '编辑'}</td>
+          <td>{this.getUserType(item)}</td>
           <td>{item.status == 1 ? <span className="label label-success">有效</span> : <span className="label label-warning">禁用</span>}</td>
           <td>{item.create_time}</td>
           <td>{item.last_login_time}</td>
           <td>
-            <Link to={'user/edit/' + item.id}>
+            {!this.state.key ? <Link to={'user/edit/' + item.id}>
               <button type="button" className="btn btn-primary btn-xs">
                 <span className="glyphicon glyphicon-edit" aria-hidden="true"></span> 编辑
               </button>
-            </Link>&nbsp;
-            <button type="button" className="btn btn-danger btn-xs" onClick={this.handleDelete.bind(this, item.id)}>
-              <span className="glyphicon glyphicon-remove" aria-hidden="true"></span> 删除
+            </Link> :
+            <button type="button" className="btn btn-success btn-xs" onClick={this.pass.bind(this, item)}>
+              <span className="glyphicon glyphicon-ok"></span>
+              通过
             </button>
+            }
+            &nbsp;
+            {!this.state.key ? <button type="button" className="btn btn-danger btn-xs" onClick={this.handleDelete.bind(this, item.id)}>
+              <span className="glyphicon glyphicon-remove" aria-hidden="true"></span> 删除
+            </button> :
+            <button type="button" className="btn btn-warning btn-xs" onClick={this.deny.bind(this, item)}>
+              <span className="glyphicon glyphicon-remove"></span>
+              拒绝
+            </button>
+            }
           </td>
         </tr>
       );
@@ -74,6 +115,10 @@ export default class extends Base {
       <div className="fk-content-wrap">
         <BreadCrumb {...this.props} />
         <div className="manage-container">
+          <Tabs activeKey={this.state.key} onSelect={this.handleSelect.bind(this)}>
+            <Tab eventKey={0} title="全　部"></Tab>
+            <Tab eventKey={3} title="审核中"></Tab>
+          </Tabs>
           <table className="table table-striped">
             <thead>
               <tr>
