50bb861cfd6ccc3f185a5b2b55fa5e24c96a4247
lizheming <secretlzm007@gmail.com>
2016-04-10 14:32:57 +0800

    fix #50

---
+++ www/static/src/common/component/editor/search.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Autobind from 'autobind-decorator';
+import Select, {Option} from 'rc-select';
+import superagent from 'superagent';
+import firekylin from 'common/util/firekylin';
+
+class Search extends React.Component {
+  state = {
+    options: []
+  }
+  fetchData(value) {
+    let req = superagent.get('/admin/api/post?keyword='+value);
+    firekylin.request(req).then(
+      resp => this.setState({options: resp.data}),
+    ).catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <Select
+          combobox
+          style={{width: '100%'}}
+          placeholder="请输入文章标题"
+          notFoundContent="暂无结果"
+          onChange={this.fetchData}
+          onSelect={this.props.onSelect}
+          filterOption={false}
+          showArrow={false}
+          dropdownStyle={{zIndex: 10000}}
+      >
+        {this.state.options.map( (opt, i) =>
+          <Option key={i} value={opt.pathname}>{opt.title}</Option>
+        )}
+      </Select>
+    );
+  }
+}
+
+Search = Autobind(Search);
+export default Search;
