53052d678bf7ed5880f85bf9da0634a59418953e
lizheming <secretlzm007@gmail.com>
2016-06-06 21:52:08 +0800

    增加主题参数配置项

--- www/static/src/admin/component/theme.jsx
+++ www/static/src/admin/component/theme.jsx
@@ -4,13 +4,14 @@ import BreadCrumb from './breadcrumb';
 import TipAction from 'common/action/tip';
 import ThemeStore from 'admin/store/theme';
 import ThemeAction from 'admin/action/theme';
-import {Form} from 'react-bootstrap-validation';
+import {Form, ValidatedInput} from 'react-bootstrap-validation';
 
 export default class extends Base {
   state = this.initialState();
   initialState() {
     return {
       list: [],
+      themeConfig: {},
       theme: window.SysConfig.options.theme || 'firekylin'
     }
   };
@@ -18,6 +19,12 @@ export default class extends Base {
   componentWillMount() {
     this.listenTo(ThemeStore, this.handleTrigger.bind(this));
     ThemeAction.list();
+    this.state.themeConfig = {};
+    if( window.SysConfig.options.themeConfig ) {
+      try{
+        this.state.themeConfig = JSON.parse(window.SysConfig.options.themeConfig);
+      } catch(e) { console.log(e) }
+    }
   }
 
   handleTrigger(data, type) {
@@ -26,6 +33,7 @@ export default class extends Base {
         this.setState({list: data});
         break;
       case 'saveThemeSuccess':
+      case 'saveThemeConfigSuccess':
         TipAction.success('设置成功');
         break;
     }
@@ -36,6 +44,83 @@ export default class extends Base {
     ThemeAction.save(this.state.theme);
   }
 
+  saveThemeConfig(vals) {
+    let data = Object.assign({}, vals, this.state.themeConfig);
+    window.SysConfig.options.themeConfig = data;
+    ThemeAction.saveThemeConfig(data);
+  }
+
+  renderConfigElement(element, i) {
+    switch(element.type) {
+      case 'url':
+      case 'text':
+      case 'email':
+      case 'textarea':
+      case 'password':
+        return (<ValidatedInput {...element} key={i}/>);
+        break;
+
+      case 'radio':
+        if(!Array.isArray(element.options)) { return null; }
+
+        return (
+          <RadioGroup {...element} key={i}>
+            {element.options.map((opt,j) => <Radio {...opt} key={j} />)}
+          </RadioGroup>
+        );
+        break;
+
+      case 'select':
+        if(!Array.isArray(element.options)) { return null; }
+
+        return (
+          <div className="form-group" key={i}>
+            <label>{element.label}</label>
+            <div>
+              <select
+                  name={element.name}
+                  value={this.state.themeConfig[element.name]}
+                  onChange={e => {
+                    this.state.themeConfig[element.name] = e.target.value;
+                    this.forceUpdate();
+                  }}
+              >
+                {element.options.map((opt, j) =>
+                  <option key={j} value={opt.value ? opt.value : opt}>{opt.label ? opt.label : opt}</option>
+                )}
+              </select>
+            </div>
+          </div>
+        );
+        break;
+    }
+  }
+
+  renderThemeConfig() {
+    if( this.state.list.length === 0 ) {
+      return null;
+    }
+
+    let theme = this.state.list.filter(theme => theme.id === this.state.theme)[0];
+    if( !theme.configElements || !Array.isArray(theme.configElements) ) {
+      return null;
+    }
+
+
+    return (
+      <div>
+        <h3 style={{marginBottom: '20px'}}>{theme.name} 主题选项</h3>
+        <Form
+            model={this.state.themeConfig}
+            className="clearfix options-general"
+            onValidSubmit={this.saveThemeConfig.bind(this)}
+        >
+          {theme.configElements.map(this.renderConfigElement.bind(this))}
+          <button type="submit" className="btn btn-primary">保存配置</button>
+        </Form>
+      </div>
+    )
+  }
   render() {
     return (
       <div className="fk-content-wrap">
@@ -62,6 +147,7 @@ export default class extends Base {
             </div>
             <button type="submit" className="btn btn-primary">提交</button>
           </Form>
+          {this.renderThemeConfig()}
         </div>
       </div>
     )
