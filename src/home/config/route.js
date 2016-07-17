3d984d3ec0c3a42c57ba833b9c8adb9f616a46dc
lizheming <secretlzm007@gmail.com>
2016-07-17 17:56:57 +0800

    fix bug #130

--- src/home/config/route.js
+++ src/home/config/route.js
@@ -2,17 +2,17 @@
  * route
  */
 export default [
-  [/^about/, 'post/page?pathname=about'],
-  [/^archives/, 'post/archive'],
-  [/^cate\/(.+)/, 'post/list?cate=:1'],
-  [/^tags\/(.+)/, 'post/list?tag=:1'],
-  [/^author\/([^/]+)/, 'post/list?name=:1'],
-  [/^tags/, 'post/tag'],
-  [/^links/, 'post/page?pathname=links'],
-  [/^rss/, 'index/rss'],
-  [/^sitemap/, 'index/sitemap'],
-  [/^search/, 'post/search'],
-  [/^page\/([^/]+)/, 'post/page?pathname=:1'],
-  [/^post\/sitemap/, 'post/sitemap'],
-  [/^post\/([^/]+)/, 'post/detail?pathname=:1']
+  [/^about$/, 'post/page?pathname=about'],
+  [/^archives$/, 'post/archive'],
+  [/^cate\/(.+)$/, 'post/list?cate=:1'],
+  [/^tags\/(.+)$/, 'post/list?tag=:1'],
+  [/^author\/([^/]+)$/, 'post/list?name=:1'],
+  [/^tags$/, 'post/tag'],
+  [/^links$/, 'post/page?pathname=links'],
+  [/^rss$/, 'index/rss'],
+  [/^sitemap$/, 'index/sitemap'],
+  [/^search$/, 'post/search'],
+  [/^page\/([^/]+)$/, 'post/page?pathname=:1'],
+  [/^post\/sitemap$/, 'post/sitemap'],
+  [/^post\/([^/]+)$/, 'post/detail?pathname=:1']
 ];
