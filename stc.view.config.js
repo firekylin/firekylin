bc868eb7bffa0969a8eaae1fb893a558a6989e15
welefen <welefen@gmail.com>
2016-08-02 17:35:59 +0800

    Use stc 2.0 instead

---
+++ stc.view.config.js
@@ -0,0 +1,38 @@
+var stc = require('stc');
+var htmlCompress = require('stc-html-compress');
+var uglify = require('stc-uglify');
+var cssCompress = require('stc-css-compress');
+var resourceVersion = require('stc-resource-version');
+var localstorage = require('stc-localstorage');
+var localstorageAdapter = require('stc-localstorage-nunjucks');
+var cssCombine = require('stc-css-combine');
+
+stc.config({
+  include: ['www/theme/firekylin.build/html', 'www/theme/firekylin/res'],
+  exclude: [],
+  outputPath: 'output.theme',
+  tpl: {
+    engine: 'nunjucks',
+    ld: ['{%', '{{', '{#'],
+    rd: ['%}', '}}', '#}'],
+  }
+});
+
+stc.workflow({
+  uglify: {plugin: uglify},
+  cssCombine: {plugin: cssCombine, include: /\.css$/},
+  cssCompress: {plugin: cssCompress},
+  //htmlCompress: {plugin: htmlCompress},
+  localstorage: {
+    include: {type: 'tpl'},
+    plugin: localstorage,
+    options: {
+      adapter: localstorageAdapter,
+      minLength : 200,
+      appId : '3e988cdb'
+    }
+  },
+  resourceVersion: {plugin: resourceVersion}
+});
+
+stc.start();
