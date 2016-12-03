7d0980339c3c2b123e6189507d54d86649c80743
xiaoping <edwardhjp@gmail.com>
2016-12-03 19:17:26 +0800

    add import for ghost

--- www/static/src/admin/component/import.jsx
+++ www/static/src/admin/component/import.jsx
@@ -8,13 +8,15 @@ import TipAction from 'common/action/tip';
 
 module.exports = class extends Base {
   state = {
-    uploading: false
+    uploading: false,
+    uploadType: ''
   };
-  handleValidSubmit(e) {
+  handleValidSubmit(type, e) {
+    this.setState({ uploadType: type }); // 上传类型指定
     this.setState({uploading: true});
     var form = new FormData();
     form.append('file', e.file[0]);
-    form.append('importor', 'wordpress');
+    form.append('importor', type);
     firekylin.upload(form).then(result => {
       TipAction.success(result.data);
       alert(result.data);
@@ -25,6 +27,34 @@ module.exports = class extends Base {
     });
   }
   render(){
+    const ghost = (
+      <div className="options-2fa" style={{borderTop: '1px solid #999', marginTop: '20px'}}>
+        <h3 style={{marginBottom: '20px'}}>上传 JSON 文件</h3>
+        <p>请上传 Ghost 中导出的 .json 文件</p>
+        <Form
+            className="clearfix form-horizonal"
+            onValidSubmit={this.handleValidSubmit.bind(this, 'ghost')}
+        >
+          <ValidatedInput
+              type="file"
+              name="file"
+              label="上传文件："
+              labelClassName="col-xs-2"
+              wrapperClassName="col-xs-10"
+              validate={files => {
+                if (FileValidator.isEmpty(files)) {
+                    return '请上传文件';
+                }
+                return true;
+              }}
+              accept="application/json"
+          />
+          <button type="submit" className="btn btn-primary">
+            上传{this.state.uploadType === 'ghost' && this.state.uploading?'中...':''}
+          </button>
+        </Form>
+      </div>
+    );
     return (
       <div className="fk-content-wrap">
         <BreadCrumb {...this.props} />
@@ -34,7 +64,7 @@ module.exports = class extends Base {
             <p>请上传 WordPress 中导出的 .xml 文件</p>
             <Form
                 className="clearfix form-horizonal"
-                onValidSubmit={this.handleValidSubmit.bind(this)}
+                onValidSubmit={this.handleValidSubmit.bind(this, 'wordpress')}
             >
               <ValidatedInput
                   type="file"
@@ -50,9 +80,12 @@ module.exports = class extends Base {
                   }}
                   accept="application/xml"
               />
-              <button type="submit" className="btn btn-primary">上传{this.state.uploading?'中...':''}</button>
+              <button type="submit" className="btn btn-primary">
+                上传{this.state.uploadType === 'wordpress' && this.state.uploading?'中...':''}
+              </button>
             </Form>
           </div>
+          { ghost }
         </div>
       </div>
     );
