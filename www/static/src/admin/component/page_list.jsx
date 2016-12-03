c8c60834e0f407d722af8b360701c45ef6260ec4
lizheming <secretlzm007@gmail.com>
2016-12-03 18:10:49 +0800

    fix #186 删除用户后文章页面列表显示不正常

--- www/static/src/admin/component/page_list.jsx
+++ www/static/src/admin/component/page_list.jsx
@@ -51,7 +51,7 @@ module.exports = class extends Base {
           <td>
             <Link to={`/page/edit/${item.id}`} title={item.title}>{item.title}</Link>
           </td>
-          <td>{item.user.display_name || item.user.name}</td>
+          <td>{item.user ? item.user.display_name || item.user : null}</td>
           <td>{this.renderStatus(item.status)}</td>
           <td>{firekylin.formatTime(item.create_time)}</td>
           <td>{firekylin.formatTime(item.update_time)}</td>
