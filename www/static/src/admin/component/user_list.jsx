e64af41b286164b226acfbf1b86f6cd81391bda0
lizheming <secretlzm007@gmail.com>
2017-02-08 18:16:02 +0800

    fix #230 编辑用户修复

--- www/static/src/admin/component/user_list.jsx
+++ www/static/src/admin/component/user_list.jsx
@@ -70,10 +70,10 @@ module.exports = class extends Base {
 
   getUserList(){
     if(this.state.loading){
-      return (<tr><td colSpan="8" className="center">加载中……</td></tr>);
+      return (<tr><td colSpan="10" className="center">加载中……</td></tr>);
     }
     if(!this.state.userList.length){
-      return (<tr><td colSpan="8" className="center">无相关用户</td></tr>);
+      return (<tr><td colSpan="10" className="center">无相关用户</td></tr>);
     }
     return this.state.userList.map(item => {
       return (
