4221055c30c51db2ccd09d1ff815b3e1580cc5cc
mojie126 <mojie126@foxmail.com>
2017-04-13 23:28:19 +0800

    新增host环境变量 可以在pm2.json的env里直接设置，防止在app/common/config/config.js里修改导致更新时被覆盖的问题

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -7,10 +7,18 @@ let portFile = think.ROOT_PATH + think.sep + 'port';
 if(think.isFile(portFile)) {
   port = fs.readFileSync(portFile, 'utf8');
 }
+
+let host;
+let hostFile = think.ROOT_PATH + think.sep + 'host';
+if(think.isFile(hostFile)) {
+	host = fs.readFileSync(hostFile, 'utf8');
+}
+
 /**
  * config
  */
 export default {
+  host: host || process.env.HOST || '0.0.0.0',
   port: port || process.env.PORT || 8360,
   resource_reg: /^(static\/|theme\/|[^\/]+\.(?!js|html|xml)\w+$)/
 };
