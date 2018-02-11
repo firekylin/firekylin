10aa58fe79af9182c814a6de7a302fed7b1f50f8
lizheming <secretlzm007@gmail.com>
2018-02-11 20:32:54 +0800

    修改路由到 common 模块下 路由配置无法写到子模块下，路由正则需要匹配 / 开头的路由，路由完整匹配需要包括 / 需要考虑正则大小写匹配

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,3 +1,26 @@
 module.exports = [
-  [/^admin/, 'admin']
+  [/^admin/, 'admin'],
+
+  [/^\/cate\/(.+)(?:\.html|\.json)$/i, 'post/list?cate=:1'],
+  [/^\/tag\/(.+)(?:\.html|\.json)$/i, 'post/list?tag=:1'],
+  [/^\/author\/([^/]+)(?:\.html|\.json)$/i, 'post/list?name=:1'],
+  [/^\/tags(?:\.html|\.json)$/i, 'post/tag'],
+  [/^\/search(?:\.html|\.json)$/i, 'post/search'],
+  [/^\/page\/([^/]+)(?:\.html|\.json)$/i, 'post/page?pathname=:1'],
+  [/^\/post\/([^/]+)(?:\.html|\.json)$/i, 'post/detail?pathname=:1'],
+
+  [/^\/about\/?$/i, 'post/page?pathname=about'],
+  [/^\/archives\/?$/i, 'post/archive'],
+  [/^\/cate\/(.+)\/?$/i, 'post/list?cate=:1'],
+  [/^\/tag\/(.+)\/?$/i, 'post/list?tag=:1'],
+  [/^\/author\/([^/]+)\/?$/i, 'post/list?name=:1'],
+  [/^\/tags\/?$/i, 'post/tag'],
+  [/^\/links\/?$/i, 'post/page?pathname=links'],
+  [/^\/rss(?:\.xml)?\/?$/i, 'index/rss'],
+  [/^\/sitemap(?:\.xml)?\/?$/i, 'index/sitemap'],
+  [/^\/search\/?$/i, 'post/search'],
+  [/^\/page\/([^/]+)\/?$/i, 'post/page?pathname=:1'],
+  [/^\/post\/sitemap\/?$/i, 'post/sitemap'],
+  [/^\/post\/([^/]+)\/?$/i, 'post/detail?pathname=:1'],
+  ['opensearch.xml', 'index/opensearch']
 ];
