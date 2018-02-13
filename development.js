b29c0c39796714808a8a7cde9aae2d07ed4140c0
lizheming <secretlzm007@gmail.com>
2018-02-13 20:23:06 +0800

    设置 APP_PATH 入口目录

--- development.js
+++ development.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const Application = require('thinkjs');
 const watcher = require('think-watcher');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
+  APP_PATH: path.join(__dirname, 'src'),
   watcher: watcher,
   env: 'development'
 });
--- production.js
+++ production.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const Application = require('thinkjs');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
+  APP_PATH: path.join(__dirname, 'src'),
   proxy: true, // use proxy
   env: 'production'
 });
