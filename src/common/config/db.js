64f950a39cc68a3b3318654be3e9c3c761eab1ce
呛水滴鱼 <874289852@qq.com>
2018-04-16 15:23:11 +0800

    db.js

---
+++ src/common/config/db.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+exports.default = {
+    "type": "mysql",
+    "adapter": {
+        "mysql": {
+            "host": "localhost",
+            "port": "3306",
+            "database": "firekylin",
+            "user": "test",
+            "password": "1234",
+            "prefix": "fk_",
+            "encoding": "utf8mb4"
+        }
+    }
+}
+    
