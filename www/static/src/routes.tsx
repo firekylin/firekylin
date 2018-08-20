b94edfc5bf99019b9dac7c36fbb9686841f94957
HuJian <624598206@qq.com>
2018-08-20 23:28:49 +0800

    style(admin/routes): delete unused Switch

--- www/static/src/routes.tsx
+++ www/static/src/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Container from './routes/container';
 import { auth } from './utils/auth';
 import Login from './routes/login/login';
