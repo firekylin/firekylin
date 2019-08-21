6d84ebb634f8b7344271b9dcf05400912d16af30
lizheming <i@imnerd.org>
2019-08-21 21:42:31 +0800

    去除uglify逻辑

--- stc.config.js
+++ stc.config.js
@@ -1,6 +1,6 @@
 var stc = require('stc');
 var htmlCompress = require('stc-html-compress');
-var uglify = require('stc-uglify');
+// var uglify = require('stc-uglify');
 var cssCompress = require('stc-css-compress');
 var resourceVersion = require('stc-resource-version');
 
@@ -17,10 +17,10 @@ stc.config({
 });
 
 stc.workflow({
-  uglify: {plugin: uglify, exclude: [/static\/src/]},
-  cssCompress: {plugin: cssCompress},
-  resourceVersion: {plugin: resourceVersion},
-  htmlCompress: {plugin: htmlCompress}
+  // uglify: {plugin: uglify, exclude: [/static\/src/]},
+  cssCompress: { plugin: cssCompress },
+  resourceVersion: { plugin: resourceVersion },
+  htmlCompress: { plugin: htmlCompress }
 });
 
 stc.start();
