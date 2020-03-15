e5261eb525d5a9a2f4cd47fac27c9835ab1cd237
lizheming <i@imnerd.org>
2020-03-15 13:23:53 +0800

    fix #741 下载更新地址不对的问题

--- www/static/src/routes/dashboard/dashboard.tsx
+++ www/static/src/routes/dashboard/dashboard.tsx
@@ -121,7 +121,7 @@ class DashBoard extends React.Component<DashBoardProps, any> {
                     <p className="bg-info update-message">
                     Firekylin <a
                     href={`https://github.com/firekylin/firekylin/blob/master/CHANGELOG.md#${(versions.needUpdate as string).replace(/\./g, '')}`}
-                    >{versions.needUpdate}</a> 已经发布，请立即 <a href="http://firekylin.org/release/latest.tar.gz"
+                    >{versions.needUpdate}</a> 已经发布，请立即 <a href="http://firekylin.org/release/v1/latest.tar.gz"
                     >下载更新</a> 或者使用 <a href="javascript:void(0)" onClick={() => this.renderUpdateConfirm()}
                     >在线更新</a>！
                     </p>
