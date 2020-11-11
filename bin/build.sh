ab1e85fa6c3be87e8f74a1f2363038858c46b0d0
lizheming <i@imnerd.org>
2020-11-11 15:26:22 +0800

    chore: remove scp

--- bin/build.sh
+++ bin/build.sh
@@ -69,10 +69,3 @@ rm -rf firekylin/;
 
 cd build;
 tar zxvfm $TARNAME;
-
-
-HOST="root@firekylin.org";
-REMOTE_TAR="/var/www/firekylin.org/www/release/v1";
-scp $TARNAME $HOST:$REMOTE_TAR;
-ssh $HOST cp $REMOTE_TAR/$TARNAME $REMOTE_TAR/latest.tar.gz;
-ssh $HOST "echo $VERSION > $REMOTE_TAR/.latest";
